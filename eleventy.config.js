6cfab25049e930b48ee28b29a465bb061e452802
Afsheen <afsheenamroliwala@gmail.com>
2025-04-29 14:10:08 +0530

    setting up md file render

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,14 @@
+import markdownIt from "markdown-it";
+
 export default async (eleventyConfig) => {
+
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true,
+  };
+  eleventyConfig.setLibrary("md", markdownIt(options));
+
   eleventyConfig.addPassthroughCopy('assets');
   
   return {
